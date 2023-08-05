0d2c953180a9c35549628a302f605d03a94373a0
kautsar <kautsarteguh2002@gmail.com>
2023-08-05 18:31:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasakb/ui/info/InfoFragment.java
@@ -0,0 +1,37 @@
+package com.example.tugasakb.ui.info;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tugasakb.databinding.FragmentInfoBinding;
+
+public class InfoFragment extends Fragment {
+
+    private FragmentInfoBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InfoViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(InfoViewModel.class);
+
+        binding = FragmentInfoBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
---
+++ app/src/main/java/com/example/tugasakb/ui/note/NoteViewModel.java
@@ -0,0 +1,8 @@
+package com.example.tugasakb.ui.note;
+
+import androidx.lifecycle.ViewModel;
+
+public class NoteViewModel extends ViewModel {
+
+
+}
